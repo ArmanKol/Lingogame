e75ecf3a42304d8e7bdc6cdeeeacb438a5cff861
ArmanKol <arman.koldaguc@gmail.com>
2020-05-30 12:30:48 +0200

    input woorden controle

--- src/main/java/hu/bep/logic/GameEngine.java
+++ src/main/java/hu/bep/logic/GameEngine.java
@@ -3,24 +3,147 @@ package hu.bep.logic;
 import hu.bep.logic.state.GameState;
 import hu.bep.logic.state.LevelState;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GameEngine {
-    private String givenWord;
+    private String wordToGuess;
     private int guessesLeft;
     private int score;
-    private Enum gameState, levelState;
+    private GameState gameState;
+    private LevelState levelState;
+    private Map<Character, Integer> charsWordToGuess = new HashMap<Character, Integer>();
 
 
     public GameEngine(){
 
     }
 
-
-
     public void start(){
         gameState = GameState.PLAYING;
         levelState= LevelState.FIVE_LETTER_WORD;
         guessesLeft = 5;
         score = 0;
+        wordToGuess = "hallo";
+        fillHashMapWithLetters(wordToGuess, charsWordToGuess);
+    }
+
+    public void roundController(String wordGuess){
+        if(wordGuessed(wordGuess)){
+            nextRound();
+        }
+
+        if(guessesLeft == 0){
+
+        }
+
+        guessesLeft--;
+    }
+
+    private void nextRound(){
+        charsWordToGuess.clear();
+        levelStateController();
+        guessesLeft = 5;
+        wordToGuess = "poepen";
+        fillHashMapWithLetters(wordToGuess, charsWordToGuess);
+    }
+
+    private void levelStateController(){
+        switch(levelState){
+            case FIVE_LETTER_WORD:
+                levelState = LevelState.SIX_LETTER_WORD;
+                break;
+            case SIX_LETTER_WORD:
+                levelState = LevelState.SEVEN_LETTER_WORD;
+                break;
+            case SEVEN_LETTER_WORD:
+                levelState = LevelState.FIVE_LETTER_WORD;
+                break;
+        }
+    }
+
+    private boolean wordGuessed(String inputWord){
+        Map<Integer, Character> charsGuessedRight = new HashMap<Integer, Character>();
+        StringBuilder guessedWord = new StringBuilder();
+
+        if(inputWord.length() <= wordToGuess.length()){
+            guessedWord = checkWord(inputWord, charsGuessedRight).get(1);
+        }else if(inputWord.length() > wordToGuess.length()){
+
+        }
+
+        if(guessedWord.toString().equals(inputWord)){
+            return true;
+        }
+
+        return false;
+    }
+
+    private Map<Integer, StringBuilder> checkWord(String inputWord, Map<Integer, Character> charsGuessedRight){
+        Map<Integer, StringBuilder> returnValues = new HashMap<Integer, StringBuilder>();
+
+        StringBuilder feedBackWord = new StringBuilder();
+        StringBuilder guessedChars = new StringBuilder();
+
+        final String ANSI_RESET = "\u001B[0m";
+        final String ANSI_RED = "\u001B[31m";
+        final String ANSI_GREEN = "\u001B[32m";
+        final String ANSI_YELLOW = "\u001B[33m";
+
+        for(int i =0; i < inputWord.length(); i++){
+            char charInputWord = inputWord.charAt(i);
+            char charWordToGuess = wordToGuess.charAt(i);
+
+            if(charInputWord == charWordToGuess){
+                feedBackWord.append(ANSI_GREEN + charInputWord + ANSI_RESET);
+                charsGuessedRight.put(i, charInputWord);
+                guessedChars.append(charInputWord);
+            }else if(charsWordToGuess.containsKey(charInputWord)){
+                if(feedBackWord.toString().contains(String.valueOf(charInputWord)) && charsWordToGuess.get(charInputWord) == 1){
+                    feedBackWord.append(ANSI_RED + charInputWord + ANSI_RESET);
+                    guessedChars.append(".");
+                }else{
+                    feedBackWord.append(ANSI_YELLOW + charInputWord + ANSI_RESET);
+                    guessedChars.append(".");
+                }
+            }else{
+                feedBackWord.append(ANSI_RED + charInputWord + ANSI_RESET);
+                guessedChars.append(".");
+            }
+        }
+
+        System.out.println("-----------");
+        System.out.println(feedBackWord);
+        System.out.println(guessedChars);
+        System.out.println("-----------");
+
+        returnValues.put(0, feedBackWord);
+        returnValues.put(1, guessedChars);
+
+        return returnValues;
+    }
+
+    private void score(StringBuilder input, StringBuilder guessedChars){
+        if(input == guessedChars){
+            score = 50;
+        }
+
+    }
+
+
+    private void fillHashMapWithLetters(String givenWord, Map<Character, Integer> map){
+        for(int i = 0; i < givenWord.length(); i++){
+            char character = givenWord.charAt(i);
+            if(map.containsKey(character)){
+                map.replace(character, map.get(character)+1);
+            }else{
+                map.put(character, 1);
+            }
+        }
+    }
+
+    public String getGivenWord(){
+        return wordToGuess;
     }
 
 }
--- src/main/java/hu/bep/presentation/GameController.java
+++ src/main/java/hu/bep/presentation/GameController.java
@@ -8,9 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 @RestController
 public class GameController{
     private static Logger logger = LogManager.getLogger(GameController.class);
@@ -18,13 +15,14 @@ public class GameController{
     @Autowired
     private WordRepository wordRepository;
 
-    @GetMapping("/lingo/randomword")
-    public String getRandom(){
+    @GetMapping("/api/randomword")
+    public String getRandomWord(){
         RandomWordGenerator randomGenerator = new RandomWordGenerator(wordRepository.getMinID(), wordRepository.getMaxID());
 
         int wordID = randomGenerator.getRandomNumber();
-        String word = wordRepository.findByID(wordID).getWord();
 
-        return word;
+        return wordRepository.findByID(wordID).getWord();
     }
+
+
 }
